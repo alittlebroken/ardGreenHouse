715f1b985f65c683bfb680a3d295b813b52666cd
Paul Lockyer <plockyer@googlemail.com>
2019-05-03 17:08:55 +0100

    Create ardGreenhouse.ino

---
+++ code/ardGreenhouse.ino
@@ -0,0 +1,31 @@
+/*
+*   ardGreenHouse.ino
+*   A basic template to use for new Arduino sketches
+*   Paul Lockyer (plockyer@googlemail.com)
+*   2019-05-03
+*   Version 0.1
+*/
+
+// Includes
+
+// Defines
+// What pins are the sensors connected to?
+#define sensorSoilMoisture A0
+#define sensorTempHumid 1
+
+// Variables/Properties
+
+
+// Methods
+
+// PROGRAM START
+void setup(){
+
+}
+
+
+void loop(){
+
+
+}
+// PROGRAM END
