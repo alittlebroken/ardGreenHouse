248930467691f4d80856886e2fe4911923e6e6c4
Paul Lockyer <plockyer@googlemail.com>
2019-05-13 15:45:42 +0100

    Added connect and disconnect

--- webapp/apps/database.app.php
+++ webapp/apps/database.app.php
@@ -29,12 +29,11 @@ class dbApp
   */
   
   // The class constructor use this to setup some basic information
-  // It should be passed in a copy of the configuration for the web app
+  // It should be passed in a copy of the configuration JSON for the web app
   public function __construct($config)
   {
-  
       // Assign  the configuration options
-      $this->_cfg = $config;
+      $this->_cfg = json_decode($config,true);
       
       // Assign the rest of the configuration options
       $this->_host = $this->_cfg["db"]["hostName"];
@@ -42,8 +41,50 @@ class dbApp
       $this->_uid = $this->_cfg["db"]["userName"];
       $this->_pwd = $this->_cfg["db"]["userPassword"];
       
+      // Get the log we wish to output to for debug or various other messages
       $this->_log = $this->_cfg["log"]["logFileName"];
+      
+      // Create the DSN for the PDO object to use
+      $this->_dsn = "mysql:hostname=" . $this->_host . ";dbname=" . $this->_pwd;
+      
+      //Create an array of the options
+      $this->_opts = [
+    	PDO::ATTR_ERRMODE				=> 		PDO::ERRMODE_EXCEPTION,
+    	PDO::ATTR_DEFAULT_FETCH_MODE	=>		PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES		=>		false
+      ];
+  }
   
+  // Send a message to the log file
+  private function writeLog($msg){
+      // Create the message to write to the log file
+      $logMessage = date('Y/m/d h:i:s');
+      $logMessage .= " [DB] ";
+      $logMessage .= $msg;
+      
+      // Append the message to the log file
+      file_put_content($this->_log,$logMessage,FILE_APPEND);  
+  }
+    
+  // Attempt to connect to the database
+  public function connect(){
+      // ALWAYS attempt this in a try catch block
+      try{
+          // Connect to the database
+          $this->_dbo = new PDO($this->_dsn,$this->_uid,$this->_pwd,$this->_opts); 
+      }catch(PDOException $e){
+          // OOPS, lets output to the log what has gone wrong
+          $this->writeLog("There was a problem connectin to the database.");
+          $this->writeLog($e->getMessage());     
+      }   
+  }
+    
+  // Dicsonnect from the database
+  public function disconnect(){
+      // Only disconnect if the object resoucre is valid
+      if($this->_dbo != null){
+         $this->_dbo = null   
+      }
   }
 
 }
