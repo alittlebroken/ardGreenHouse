7cf7f73d176d3b660b1aeb01ac2720761fa53017
Paul Lockyer <plockyer@googlemail.com>
2020-09-30 13:18:43 +0100

    Bug Fixes
    1. Set the update frequency var to unsigned long from int
    2. Set the previous time to only update once the timer has triggered
    3. Set the Update method to set triggered to false each time the timer did not trigger as otherwise it was left triggered each update

--- code/Timer.h
+++ code/Timer.h
@@ -1,45 +1,41 @@
-/*
-*   Timer.h
-*   A simple timer class, 
-*   Paul Lockyer (plockyer@googlemail.com)
-*   2020-09-30
-*/
-
- class Timer {
+class Timer {
 
     private:
-        unsigned long _currTime;                         // Records the current time at the start of the update loop
-        unsigned long _prevTime;                         // Records the last time the update loop was run
-        long _update_freq;                               // How often does the timer need to trigger
+        unsigned long _currTime;                     // Records the current time at the start of the update loop
+        unsigned long _prevTime;                     // Records the last time the update loop was run
+        unsigned long _update_freq;                  // How often does the timer need to trigger
         
         
     public:
-        Timer(long updateFrequency);                 // Class constructer. Just takes in how often the timer triggers
+        Timer(unsigned long updateFrequency);        // Class constructer. Just takes in how often the timer triggers
         Update();                                    // Updates the timer internals
-        bool timerTriggered;                              // Indicates to the outside world if the timer has triggered
+        bool timerTriggered;                         // Indicates to the outside world if the timer has triggered
         
 };
 
 // Class constructer - sets up the base properties
-Timer::Timer(long updateFrequency)
+Timer::Timer(unsigned long updateFrequency)
 {
     this->_update_freq = updateFrequency;
-    this->timerTriggered = false;
+    timerTriggered = false;
 }
 
 // Updates all internals each time it is called
 Timer::Update()
 {
-    // Set the current time
+
+    // Set the current time for the update
     this->_currTime = millis();
-    
-    // Check if the time has been triggered or not
-    if(this->_currTime - this->_prevTime > this->_update_freq)
+
+    // Check if we have just triggered, otherwise set us to not have triggered
+    if(this->_currTime - this->_prevTime >= this->_update_freq)
     {
         // We have been triggered and we need to set the triggered flag
-        this->timerTriggered = true;
+        _prevTime = _currTime;
+        timerTriggered = true;
+    }else{
+        timerTriggered = false;
     }
-    
-    // Set the previous time this function was called
-    this->_prevTime = this->_currTime;
+      
+    return;
 }
